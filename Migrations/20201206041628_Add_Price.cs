9d866a02456163fc90e3aeda875c3383563946ef
LinChinHuang <61148811+LinChinHuang@users.noreply.github.com>
2020-12-06 12:47:12 +0800

    Add_Price

---
+++ Migrations/20201206041628_Add_Price.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASPNetCore5Demo.Migrations
+{
+    public partial class Add_Price : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Course");
+        }
+    }
+}
